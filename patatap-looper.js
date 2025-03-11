cceca8ed2c14678bffc0adc279c710ab70d7726d
Nitish Sainani <37032957+nitishsainani@users.noreply.github.com>
2025-03-12 04:39:03 +0530

    Fixed the code. removed jquery and _

--- patatap-looper.js
+++ patatap-looper.js
@@ -1,71 +1,109 @@
-(function(exports, $container){
+(function(exports) {
   var SL = {
-      beatsPerLoop: 16,
-      beatsPerMinute: 200,
-      _loops: {},
-      _stop: false,
-      add: function(name, beats) {
-        this._loops[name] = beats.split(' ');
-        return this;
-      },
-      remove: function(name, beats) {
-        delete this._loops[name];
-      },
-      list: function() {
-        var count = 0;
-        for (key in this._loops) {
-          console.log(key + ': ' + this._loops[key].join(' '));
-          count++;
-        }
-        console.log(count + ' loop(s)');
-      },
-      clear: function() {
-        this._loops = {};
-      },
-      stop: function() {
-        this._stop = true;
-      },
-      start: function() {
-        this._stop = false
-        superloop(0, this.beatsPerLoop, this.beatsPerMinute);
-      }
-  }
+    beatsPerLoop: 16,
+    beatsPerMinute: 200,
+    _loops: {},
+    _stop: false,
 
-  function playBeat(loop, beat) {
-    if(loop[beat] !== '-'){
-      var e = jQuery.Event("keydown");
-      try {
-        e.which = loop[beat].toUpperCase().charCodeAt(0);
-      } catch(error) {
-        console.warn('Typo at beat ' + beat + '');
+    add: function(name, beats) {
+      this._loops[name] = beats.split(' ');
+      return this;
+    },
+
+    remove: function(name) {
+      delete this._loops[name];
+    },
+
+    list: function() {
+      var count = 0;
+      for (var key in this._loops) {
+        console.log(key + ': ' + this._loops[key].join(' '));
+        count++;
       }
-      $("input").val(String.fromCharCode(e.which));
-      $("html").trigger(e);
+      console.log(count + ' loop(s)');
+    },
+
+    clear: function() {
+      this._loops = {};
+    },
+
+    stop: function() {
+      this._stop = true;
+    },
+
+    start: function() {
+      this._stop = false;
+      superloop(0, this.beatsPerLoop, this.beatsPerMinute);
+    }
+  };
+
+  /**
+   * Plays the character at the specified beat in a loop array.
+   * This version uses plain JavaScript instead of jQuery.
+   */
+  function playBeat(loop, beat) {
+    var character = loop[beat];
+    if (character && character !== '-') {
+      // Convert the character to upper case and get its char code.
+      var keyChar = character.toUpperCase();
+      var keyCode = keyChar.charCodeAt(0);
+
+      // Try to fire a keydown event in plain JS:
+      try {
+        var event = new KeyboardEvent('keydown', {
+          bubbles: true,
+          cancelable: true,
+          key: keyChar,
+          code: 'Key' + keyChar,
+          which: keyCode,
+          keyCode: keyCode
+        });
+        
+        // Update the first <input> we find in the DOM (if any).
+        var input = document.querySelector('input');
+        if (input) {
+          input.value = keyChar;
+        }
+        
+        // Dispatch the event on <html> (document.documentElement).
+        document.documentElement.dispatchEvent(event);
+      } catch(error) {
+        console.warn('Typo at beat ' + beat);
+      }
     }
   }
 
+  /**
+   * The main loop function, called recursively.
+   */
   function superloop(beat, bpl, bpm) {
-    _.each(SL._loops, function(loop) {
+    // Loop over each named loop in SL._loops and play the current beat.
+    Object.keys(SL._loops).forEach(function(key) {
+      var loop = SL._loops[key];
       playBeat(loop, beat);
     });
-    if (SL._stop)
-      return
-    setTimeout(function(){
-      var nextBeat = (beat+1)%bpl;
+
+    if (SL._stop) return;
+
+    setTimeout(function() {
+      var nextBeat = (beat + 1) % bpl;
       superloop(nextBeat, bpl, bpm);
     }, (1000 * 60) / bpm);
   }
-  
+
+  // Start immediately by default.
   superloop(0, SL.beatsPerLoop, SL.beatsPerMinute);
+
+  // Expose the superloops object so we can call `superloops.add(...)`, etc.
   exports.superloops = SL;
-  if ($container) {
-    $container.append('<div style="position:fixed; top: 10px; left: 10px;font-family:helvetica; font-size: 30px;color: white; padding: 20px;background-color: rgba(0,0,0,0.2);border: 5px solid black;">github.com/reimertz/patatap-looper</div>');
-  }
-  console.clear()
-})(window, $("html"));
 
+  // Clear the console (optional).
+  console.clear();
 
+})(window);
 
-superloops.add('base', 'e - - - e - - - e - - - e - - -')
-          .add('snare', '- - o - - - o - - - o - - - o -')
-          .add('love', '- p - h - e - a - r - t - m - -');
+// EXAMPLE USAGE:
+superloops
+  .add('base',  'e - - - e - - - e - - - e - - -')
+  .add('snare', '- - o - - - o - - - o - - - o -')
+  .add('love',  '- p - h - e - a - r - t - m - -');
